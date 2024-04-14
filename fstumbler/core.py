fybx/fstumbler

commit e4cc039d44f281f3b13a79996a743cd16b7e525f

slightly improved tumbling logic
i'm not sure but this indexes way less files then the first candidate; but it did well on normal test folders. so i assume that it's trustable :D

Signed-off-by: Ferit Yiğit BALABAN <fyb@fybx.dev>

fstumbler/core.py
@@ -43,14 +43,19 @@ def tumble(root_directory: str) -> Optional[Node]:
     root = Node(os.path.dirname(full_path), name)
     pointer = root
     
+    parent_dirs = []
+    last_parent = ''
     for rootname, dirnames, filenames in os.walk(full_path):
-        for filename in filenames:
-            node = Node(rootname, filename, False)
+        if rootname not in parent_dirs:
+            parent_dirs = [os.path.join(rootname, name) for name in dirnames]
+            last_parent = rootname
+        else:
+            node = Node(last_parent, os.path.basename(rootname))
             pointer.next = node
             pointer = node
         
-        for dirname in dirnames:
-            node = Node(rootname, dirname)
+        for filename in filenames:
+            node = Node(rootname, filename, directory=False)
             pointer.next = node
             pointer = node
     

commit 590119e0849470109b096e7c5caea6b74166f37b

enh: fix bug introduced in e4cc039
this commit fixes the bug, while also potentially improving the performance by changing the main tumbler approach I first went for.

Signed-off-by: Ferit Yiğit BALABAN <fyb@fybx.dev>

fstumbler/core.py
@@ -40,24 +40,20 @@ def tumble(root_directory: str) -> Node:
     if not os.path.exists(root_directory):
         raise FileNotFoundError(root_directory)
     
-    full_path = os.path.abspath(root_directory)
-    name = os.path.basename(full_path)
-    root = Node(os.path.dirname(full_path), name)
+    root = Node(os.path.dirname(root_directory), os.path.basename(root_directory), directory=True)
     pointer = root
     
-    parent_dirs = []
-    last_parent = ''
-    for rootname, dirnames, filenames in os.walk(full_path):
-        if rootname not in parent_dirs:
-            parent_dirs = [os.path.join(rootname, name) for name in dirnames]
-            last_parent = rootname
+    first = True
+    for rootname, _, filenames in os.walk(os.path.abspath(root_directory)):
+        if first:
+            first = False
         else:
-            node = Node(last_parent, os.path.basename(rootname))
+            node = Node(os.path.dirname(rootname), os.path.basename(rootname), directory=True)
             pointer.next = node
             pointer = node
         
-        for filename in filenames:
-            node = Node(rootname, filename, directory=False)
+        for name in filenames:
+            node = Node(rootname, name, directory=False)
             pointer.next = node
             pointer = node
     